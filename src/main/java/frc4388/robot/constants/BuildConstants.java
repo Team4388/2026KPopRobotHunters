11fa322f5922f7d432c0db927c77ec061c44eb81
Michael Mikovsky <mira.j.maroni@gmail.com>
2026-01-19 18:34:14 -0700

    Autos Continued

--- src/main/java/frc4388/robot/constants/BuildConstants.java
+++ src/main/java/frc4388/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026KPopRobotHunters";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "d30c807b0735998c3181d2fa0c83fde4ab9b3a6c";
-  public static final String GIT_DATE = "2026-01-19 14:17:15 MST";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "7c8593a9f72fe5515886c9d6c122b14789652210";
+  public static final String GIT_DATE = "2026-01-19 15:21:40 MST";
   public static final String GIT_BRANCH = "Autorotate";
-  public static final String BUILD_DATE = "2026-01-19 15:17:53 MST";
-  public static final long BUILD_UNIX_TIME = 1768861073727L;
+  public static final String BUILD_DATE = "2026-01-19 18:33:26 MST";
+  public static final long BUILD_UNIX_TIME = 1768872806551L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
