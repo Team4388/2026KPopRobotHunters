7b46b7a1a30820e2e754c8b8335bd4207dcddf77
Michael Mikovsky <shikharemail8@gmail.com>
2026-02-03 16:36:09 -0800

    Better swereve offsets

--- src/main/java/frc4388/robot/constants/BuildConstants.java
+++ src/main/java/frc4388/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026KPopRobotHunters";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 30;
-  public static final String GIT_SHA = "de0952c14a77e2b7da51b7af3a3833d495c2a3f4";
-  public static final String GIT_DATE = "2026-02-02 19:41:24 MST";
+  public static final int GIT_REVISION = 31;
+  public static final String GIT_SHA = "9fcc5ffe9f52bb7c311e0cb573061158ec2794ce";
+  public static final String GIT_DATE = "2026-02-02 19:46:18 MST";
   public static final String GIT_BRANCH = "Subsystem-Boilerplate";
-  public static final String BUILD_DATE = "2026-02-02 19:42:58 MST";
-  public static final long BUILD_UNIX_TIME = 1770086578285L;
+  public static final String BUILD_DATE = "2026-02-03 17:30:18 MST";
+  public static final long BUILD_UNIX_TIME = 1770165018277L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc4388/robot/subsystems/swerve/SwerveDriveConstants.java
+++ src/main/java/frc4388/robot/subsystems/swerve/SwerveDriveConstants.java
@@ -80,7 +80,7 @@ public final class SwerveDriveConstants {
 
     private static final class ModuleSpecificConstants { //2025
         //Front Left
-        private static final Angle FRONT_LEFT_ENCODER_OFFSET = Rotations.of(0.49707+0.35);
+        private static final Angle FRONT_LEFT_ENCODER_OFFSET = Rotations.of(0.49707+0.350-0.03+0.0134+0.06-0.043);
         private static final boolean FRONT_LEFT_DRIVE_MOTOR_INVERTED = false;
         private static final boolean FRONT_LEFT_STEER_MOTOR_INVERTED = true;
         private static final boolean FRONT_LEFT_ENCODER_INVERTED = false;
@@ -88,7 +88,7 @@ public final class SwerveDriveConstants {
         private static final Distance FRONT_LEFT_YPOS = Inches.of(HALF_HEIGHT);
         
         //Front Right
-        private static final Angle FRONT_RIGHT_ENCODER_OFFSET = Rotations.of(0.465332+0.3);
+        private static final Angle FRONT_RIGHT_ENCODER_OFFSET = Rotations.of(0.465332+0.3+0.003174-0.0103);
         private static final boolean FRONT_RIGHT_DRIVE_MOTOR_INVERTED = true;
         private static final boolean FRONT_RIGHT_STEER_MOTOR_INVERTED = true;
         private static final boolean FRONT_RIGHT_ENCODER_INVERTED = false;
@@ -96,7 +96,7 @@ public final class SwerveDriveConstants {
         private static final Distance FRONT_RIGHT_YPOS = Inches.of(-HALF_HEIGHT);
 
         //Back Left
-        private static final Angle BACK_LEFT_ENCODER_OFFSET = Rotations.of(0.023438+0.5);
+        private static final Angle BACK_LEFT_ENCODER_OFFSET = Rotations.of(0.023438+0.5+0.0168-0.00562);
         private static final boolean BACK_LEFT_DRIVE_MOTOR_INVERTED = false;
         private static final boolean BACK_LEFT_STEER_MOTOR_INVERTED = true;
         private static final boolean BACK_LEFT_ENCODER_INVERTED = false;
@@ -104,7 +104,7 @@ public final class SwerveDriveConstants {
         private static final Distance BACK_LEFT_YPOS = Inches.of(HALF_HEIGHT);
         
         //Back Right
-        private static final Angle BACK_RIGHT_ENCODER_OFFSET = Rotations.of(0.029541+0.05);
+        private static final Angle BACK_RIGHT_ENCODER_OFFSET = Rotations.of(0.029541+0.05-0.002197-0.00366);
         private static final boolean BACK_RIGHT_DRIVE_MOTOR_INVERTED = false;
         private static final boolean BACK_RIGHT_STEER_MOTOR_INVERTED = true;
         private static final boolean BACK_RIGHT_ENCODER_INVERTED = false;
