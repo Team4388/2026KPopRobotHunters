53bea5a004633c2e110616e34bed117180a1a545
Shikhar <mira.j.maroni@gmail.com>
2026-02-28 14:00:12 -0700

    fixed auto score 1 to move arm back

--- src/main/java/frc4388/robot/constants/BuildConstants.java
+++ src/main/java/frc4388/robot/constants/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026KPopRobotHunters";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 127;
-  public static final String GIT_SHA = "e1aa43d596bdcd524a75e05c72928658debd97a0";
-  public static final String GIT_DATE = "2026-02-28 13:15:57 MST";
+  public static final int GIT_REVISION = 128;
+  public static final String GIT_SHA = "a769953c22bf4140820cc5a6e6457a2c0f57774a";
+  public static final String GIT_DATE = "2026-02-28 13:36:49 MST";
   public static final String GIT_BRANCH = "shikhar-op-controls";
-  public static final String BUILD_DATE = "2026-02-28 13:31:13 MST";
-  public static final long BUILD_UNIX_TIME = 1772310673146L;
+  public static final String BUILD_DATE = "2026-02-28 13:52:47 MST";
+  public static final long BUILD_UNIX_TIME = 1772311967522L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
