0bfa170cb975d3492fcdbfc5fa8b9dcd73cc22d5
Shikhar <mira.j.maroni@gmail.com>
2026-03-03 00:07:07 -0700

    Rename

--- src/main/java/frc4388/robot/constants/BuildConstants.java
+++ src/main/java/frc4388/robot/constants/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026KPopRobotHunters";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 130;
-  public static final String GIT_SHA = "7064364dc8a8efab27ad1991e4af6ce06a523de3";
-  public static final String GIT_DATE = "2026-03-02 18:01:46 MST";
+  public static final int GIT_REVISION = 131;
+  public static final String GIT_SHA = "8cca0f81d1e8b1c74308269bacdcbf0ee6c37d75";
+  public static final String GIT_DATE = "2026-03-03 00:05:21 MST";
   public static final String GIT_BRANCH = "shikhar-op-controls";
-  public static final String BUILD_DATE = "2026-03-02 19:33:31 MST";
-  public static final long BUILD_UNIX_TIME = 1772505211993L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-03 00:06:32 MST";
+  public static final long BUILD_UNIX_TIME = 1772521592376L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
